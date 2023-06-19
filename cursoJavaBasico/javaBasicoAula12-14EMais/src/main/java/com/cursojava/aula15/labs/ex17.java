07bdbd6c51471e088373708facde47bebfd515bb
Jean Carlos <jcpds14@gmail.com>
2023-06-19 18:14:33 -0300

    ex17

---
+++ cursoJavaBasico/javaBasicoAula12-14EMais/src/main/java/com/cursojava/aula15/labs/ex17.java
@@ -0,0 +1,23 @@
+package com.cursojava.aula15.labs;
+
+import javax.swing.*;
+
+public class ex17 {
+    public static void main(String[] args) {
+        String inputYear = JOptionPane.showInputDialog("Digite o ano: ");
+        int year = Integer.parseInt(inputYear);
+
+        boolean isLeapYear = false;
+
+        if(year % 4 == 0){
+            if(year % 100 == 0 && year % 400 == 0){
+                isLeapYear = true;
+            }
+        }
+        if (isLeapYear) {
+            JOptionPane.showMessageDialog(null, year + " é um ano bissexto.");
+        } else {
+            JOptionPane.showMessageDialog(null, year + " não é um ano bissexto.");
+        }
+    }
+}
